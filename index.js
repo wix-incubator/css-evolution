07d90167221849557f7871a6605b5a3e9620eefe
Avi Marcus <avim@wix.com>
2020-01-06 11:59:50 +0200

    wnpm added should publish to npm

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-const puppeteer = require('puppeteer');
-const genetic = require('fs').readFileSync('./public/genetic.js')
-const browserReady = puppeteer.launch();
-module.exports = async function evolve({timeout, semantic, legacy, style, pre, initial}) {
-    const browser = await browserReady;
-    const page = await browser.newPage();
-    page.evaluate(`(() => {
+const chromium = require("chrome-aws-lambda");
+const genetic = require("fs").readFileSync("./public/genetic.js");
+
+let browserReady = null;
+function getBrowser() {
+  browserReady =
+    browserReady ||
+    new Promise(async resolve => {
+      const browser = await chromium.puppeteer.launch({
+        executablePath: await chromium.executablePath,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless
+      });
+      resolve(browser);
+    });
+  return browserReady;
+}
+module.exports = async function evolve({ timeout, semantic, legacy, style, pre, initial }) {
+  const browser = await getBrowser();
+  const page = await browser.newPage();
+  page.evaluate(`(() => {
         ${genetic};
         window.geneticCSS = geneticCSS;
     })()`);
-    const res = await page.evaluate(async (timeout, semantic, legacy, style, pre, initial) => {
-        let keepEvolving = true;
-        let evolve = await geneticCSS.geneticCSS({semantic, legacy, style, pre, initial});
-        setTimeout(() => keepEvolving = false, timeout)
-        let res;
-        while (keepEvolving) {
-            res = await evolve()
-        }
-        return res;
-    },timeout, semantic, legacy, style, pre, initial)
-    
-    //  await page.title();
-    console.log(res)
-    await page.close();
-    return res;
-}
+  const res = await page.evaluate(
+    async (timeout, semantic, legacy, style, pre, initial) => {
+      let keepEvolving = true;
+      let evolve = await geneticCSS.geneticCSS({
+        semantic,
+        legacy,
+        style,
+        pre,
+        initial
+      });
+      setTimeout(() => (keepEvolving = false), timeout);
+      let res;
+      while (keepEvolving) {
+        res = await evolve();
+      }
+      return res;
+    },
+    timeout,
+    semantic,
+    legacy,
+    style,
+    pre,
+    initial
+  );
+
+  //  await page.title();
+  console.log(res);
+  await page.close();
+  return res;
+};
